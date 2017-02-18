d374297aa113ce1ecf6fef05466277e8eb9147a4
Topbit Du <topbit.du@gmail.com>
2017-02-18 08:26:47 +0800

    1, Improve the Engine class to remove the un-used source codes.

--- lib/wechat/validator/engine.rb
+++ lib/wechat/validator/engine.rb
@@ -3,16 +3,6 @@ module Wechat
 
     class Engine < ::Rails::Engine
 
-      #config.autoload_paths += %W(
-      #  #{config.root}/lib
-      #  #{config.root}/app/controllers/wechat/validator/concerns
-      #)
-
-      #config.eager_load_paths += %W(
-      #  #{config.root}/lib
-      #  #{config.root}/app/controllers/wechat/validator/concerns
-      #)
-
       isolate_namespace Wechat::Validator
 
     end
